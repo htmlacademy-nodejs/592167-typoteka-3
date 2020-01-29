709c90637b489a89f581cd4cbba05690cd202721
Agafonov Alexey <agafonov@fgosniias.ru>
2020-01-29 12:41:46 +0300

    Выполняет задание module1-task3 личного проекта.

---
+++ src/service/cli/generate.js
@@ -0,0 +1,120 @@
+'use strict';
+
+const fs = require(`fs`);
+const {getRandomInt, shuffle} = require(`../../utils`);
+const {ExitCode} = require(`../../constants`);
+
+const FILE_NAME = `mocks.json`;
+const DEFAULT_OFFER = 1;
+const MAX_OFFER = 1000;
+
+const TITLES = [
+  `Ёлки. История деревьев`,
+  `Как перестать беспокоиться и начать жить`,
+  `Как достигнуть успеха не вставая с кресла`,
+  `Обзор новейшего смартфона`,
+  `Лучше рок-музыканты 20-века`,
+  `Как начать программировать`,
+  `Учим HTML и CSS`,
+  `Что такое золотое сечение`,
+  `Как собрать камни бесконечности`,
+  `Борьба с прокрастинацией`,
+  `Рок — это протест`,
+  `Самый лучший музыкальный альбом этого года`,
+];
+
+const ANNOUNCEMENT_AND_FULL_TEXT = [
+  `Ёлки — это не просто красивое дерево. Это прочная древесина.`,
+  `Первая большая ёлка была установлена только в 1938 году.`,
+  `Вы можете достичь всего. Стоит только немного постараться и запастись книгами.`,
+  `Этот смартфон — настоящая находка. Большой и яркий экран, мощнейший процессор — всё это в небольшом гаджете.`,
+  `Золотое сечение — соотношение двух величин, гармоническая пропорция.`,
+  `Собрать камни бесконечности легко, если вы прирожденный герой.`,
+  `Освоить вёрстку несложно. Возьмите книгу новую книгу и закрепите все упражнения на практике.`,
+  `Бороться с прокрастинацией несложно. Просто действуйте. Маленькими шагами.`,
+  `Программировать не настолько сложно, как об этом говорят.`,
+  `Простые ежедневные упражнения помогут достичь успеха.`,
+  `Это один из лучших рок-музыкантов.`,
+  `Он написал больше 30 хитов.`,
+  `Из под его пера вышло 8 платиновых альбомов.`,
+  `Процессор заслуживает особого внимания. Он обязательно понравится геймерам со стажем.`,
+  `Рок-музыка всегда ассоциировалась с протестами. Так ли это на самом деле?`,
+  `Достичь успеха помогут ежедневные повторения.`,
+  `Помните, небольшое количество ежедневных упражнений лучше, чем один раз, но много.`,
+  `Как начать действовать? Для начала просто соберитесь.`,
+  `Игры и программирование разные вещи. Не стоит идти в программисты, если вам нравится только игры.`,
+  `Альбом стал настоящим открытием года. Мощные гитарные рифы и скоростные соло-партии не дадут заскучать.`,
+];
+
+const CATEGORIES = [
+  `Деревья`,
+  `За жизнь`,
+  `Без рамки`,
+  `Разное`,
+  `IT`,
+  `Музыка`,
+  `Кино`,
+  `Программирование`,
+  `Железо`,
+];
+
+const Annonce = {
+  min: 1,
+  max: 5,
+};
+
+const FullText = {
+  min: 1,
+  max: ANNOUNCEMENT_AND_FULL_TEXT.length - 1,
+};
+
+const Category = {
+  min: 1,
+  max: CATEGORIES.length - 1,
+};
+
+const writeToFile = (fileName, content) => {
+  fs.writeFile(fileName, content, (err) => {
+    if (err) {
+      return console.error(`Can't write data to file...`)
+    }
+
+    return console.info(`Operation success. File created.`);
+    process.exit(ExitCode.error);
+  })
+};
+
+const getRandomDate = () => {
+  const day = new Date();
+  const threeMonth = 90;
+  day.setDate(day.getDate() - getRandomInt(0, threeMonth));
+  return day;
+};
+
+const generateOffers = (count) => (
+  Array(count).fill({}).map(() => ({
+    title: TITLES[getRandomInt(0, TITLES.length - 1)],
+    createDate: getRandomDate().toLocaleString(),
+    annonce: shuffle(ANNOUNCEMENT_AND_FULL_TEXT).slice(0, getRandomInt(Annonce.min, Annonce.max)).join(` `),
+    fullText: shuffle(ANNOUNCEMENT_AND_FULL_TEXT).slice(0, getRandomInt(FullText.min, FullText.max)).join(` `),
+    category: shuffle(CATEGORIES).slice(0, getRandomInt(Category.min, Category.max)),
+  }))
+);
+
+const generateMocks = (arg) => {
+  const [count] = arg;
+  const countOffer = Number.parseInt(count, 10) || DEFAULT_OFFER;
+  if (countOffer > MAX_OFFER) {
+    console.error(`Не больше 1000 публикаций`);
+    process.exit(ExitCode.error);
+  }
+  const content = JSON.stringify(generateOffers(countOffer));
+  writeToFile(FILE_NAME, content);
+};
+
+module.exports = {
+  name: `--generate`,
+  run(arg) {
+    generateMocks(arg);
+  }
+};
