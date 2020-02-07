6f1b96bf28ea809826c446f766327d462fb15782
Agafonov Alexey <agafonov@fgosniias.ru>
2020-02-07 14:33:12 +0300

    Добавляет набор кодов ответа сервера.

--- src/constants.js
+++ src/constants.js
@@ -5,11 +5,20 @@ const ExitCode = {
   ERROR: 1,
 };
 
+const HttpCode = {
+  OK: 200,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER_ERROR: 500,
+  FORBIDDEN: 403,
+  UNAUTHORIZED: 401,
+};
+
 const DEFAULT_COMMAND = `--help`;
 const USER_ARGV_INDEX = 2;
 
 module.exports = {
   ExitCode,
+  HttpCode,
   DEFAULT_COMMAND,
   USER_ARGV_INDEX,
 };
