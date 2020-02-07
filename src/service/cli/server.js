bd36702e1c3c7f02cc13db29051b35ab8d8a8cb4
Agafonov Alexey <agafonov@fgosniias.ru>
2020-02-07 14:43:06 +0300

    Добавляет обработку обращения на сервер: если моковые данные присутствуют, то отображаем их на странице, если нет - то выводим текст not found.

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -4,8 +4,50 @@ const fs = require(`fs`).promises;
 const http = require(`http`);
 const chalk = require(`chalk`);
 
+const {HttpCode} = require(`../../constants`);
+
 const DEFAULT_PORT = 3000;
 const FILE_NAME = `mock.json`;
+
+const sendResponse = (res, statusCode, message) => {
+  const template = `
+  <!Doctype html>
+    <html lang="ru">
+        <head>
+            <title>Typoteka</title>
+        </head>
+        <body>
+            ${message}
+        </body>
+    </html>
+  `.trim();
+
+  res.statusCode = statusCode;
+  res.writeHead(statusCode, {
+    'Content-Type': `text/html; charset=UTF-8`,
+  });
+  res.end(template);
+};
+
+const onClientConnect = async (req, res) => {
+  const notFoundMessageText = `Not found`;
+
+  switch (req.url) {
+    case `/`:
+      try {
+        const content = await fs.readFile(FILE_NAME);
+        const mocks = JSON.parse(content);
+        const message = mocks.map((elem) => `<li>${elem.title}</li>`).join(``);
+        sendResponse(res, HttpCode.OK, `<ul>${message}</ul>`);
+      } catch (err) {
+        sendResponse(res, HttpCode.NOT_FOUND, notFoundMessageText);
+      }
+      break;
+
+    default:
+      sendResponse(res, HttpCode.NOT_FOUND, notFoundMessageText);
+  }
+};
 
 const startServer = async (args) => {
   const [customPort] = args;
