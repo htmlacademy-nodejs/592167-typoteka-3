70b32bce58371124e41c1277e8d8aa8c7086000f
Agafonov Alexey <agafonov@fgosniias.ru>
2020-02-10 10:39:08 +0300

    Добавляет описание команды --server в файл help.

--- src/service/cli/help.js
+++ src/service/cli/help.js
@@ -3,12 +3,14 @@
 const chalk = require(`chalk`);
 
 const helpText = () => {
-  console.log(chalk.gray(`Программа формирует файл с данными для API.
+  console.log(chalk.gray(`Программа запускает http-сервер и формирует файл с данными для API.
     
     Команды:
     --version:            выводит номер версии
     --help:               печатает этот текст
-    --generate <count>    формирует файл mock.json`));
+    --generate <count>    формирует файл mock.json
+    --server <count>      запускает http-сервер на указанном порту
+    `));
 };
 
 module.exports = {
