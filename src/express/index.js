d5f5cfe86994226cd2f3d40b1f4f2f03c8000fef
Agafonov Alexey <agafonov@fgosniias.ru>
2020-02-25 19:13:14 +0300

    Переносит объявление переменной port ближе к месту её использования.

--- src/express/index.js
+++ src/express/index.js
@@ -1,13 +1,12 @@
 'use strict';
 
-const port = 8080;
-
 const express = require(`express`);
 const {responseContext} = require(`./routes/index`);
 
 const app = express();
 responseContext(app);
 
+const port = 8080;
 app.listen(port, () => {
   console.log(`Сервер запущен на порту: ${port}`);
 });
