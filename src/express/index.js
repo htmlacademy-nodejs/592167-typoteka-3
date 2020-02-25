23ce5619d85cd7aa87ddb38b16ffcb369bc5abbe
Agafonov Alexey <agafonov@fgosniias.ru>
2020-02-25 12:53:49 +0300

    Регистрирует список маршрутов. Добавляет файлы с маршрутами: my и offers.

--- src/express/index.js
+++ src/express/index.js
@@ -3,10 +3,24 @@
 const port = 8080;
 
 const express = require(`express`);
+const myRoutes = require(`./routes/my`);
+const offersRoutes = require(`./routes/offers`);
 
 const app = express();
 
+app.use(`/my`, myRoutes);
+app.use(`/offers`, offersRoutes);
+
 app.get(`/`, (req, res) => {
+  res.send(req.url);
+});
+app.get(`/register`, (req, res) => {
+  res.send(req.url);
+});
+app.get(`/login`, (req, res) => {
+  res.send(req.url);
+});
+app.get(`/search`, (req, res) => {
   res.send(req.url);
 });
 
