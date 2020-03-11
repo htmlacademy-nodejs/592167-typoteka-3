0e94e21c81cb890cc76925f14aad8fde11c62713
Agafonov Alexey <agafonov@fgosniias.ru>
2020-03-11 17:38:38 +0300

    Выполняет рефакторинг кода: вместо http сервера используем express.

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -1,65 +1,23 @@
 'use strict';
 
 const fs = require(`fs`).promises;
-const http = require(`http`);
+const express = require(`express`);
 const chalk = require(`chalk`);
 
 const {HttpCode, MOCK_FILE_NAME} = require(`../../constants`);
+const {initializeRoutes} = require(`../routes/index`);
 
 const DEFAULT_PORT = 3000;
-
-const sendResponse = (res, statusCode, message) => {
-  res.statusCode = statusCode;
-  res.writeHead(statusCode, {
-    'Content-Type': `text/html; charset=UTF-8`,
-  });
-
-  const responseBoby = `
-  <!Doctype html>
-    <html lang="ru">
-        <head>
-            <title>Typoteka</title>
-        </head>
-        <body>
-            ${message}
-        </body>
-    </html>
-  `.trim();
-
-  res.end(responseBoby);
-};
-
-const onClientConnect = async (req, res) => {
-  const notFoundMessageText = `Not found`;
-
-  switch (req.url) {
-    case `/`:
-      try {
-        const content = await fs.readFile(MOCK_FILE_NAME);
-        const message = JSON.parse(content).map((elem) => `<li>${elem.title}</li>`).join(``);
-        sendResponse(res, HttpCode.OK, `<ul>${message}</ul>`);
-      } catch (err) {
-        sendResponse(res, HttpCode.NOT_FOUND, notFoundMessageText);
-      }
-      break;
-
-    default:
-      sendResponse(res, HttpCode.NOT_FOUND, notFoundMessageText);
-  }
-};
+const app = express();
 
 const startServer = async (args) => {
   const [customPort] = args;
   const port = Number.parseInt(customPort, 10) || DEFAULT_PORT;
-  http.createServer(onClientConnect)
-    .listen(port)
-    .on(`listening`, (err) => {
-      if (err) {
-        return console.error(chalk.red(`Ошибка при создании сервера`, err));
-      }
 
-      return console.info(chalk.green(`Ожидаю соединений по порту ${port}`));
-    });
+  initializeRoutes(app);
+  app.listen(port, () => {
+    console.log(`Сервер запущен на порту: ${port}`);
+  })
 };
 
 module.exports = {
---
+++ src/service/routes/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const initializeRoutes = (app) => {};
+
+module.exports = {
+  initializeRoutes
+};
