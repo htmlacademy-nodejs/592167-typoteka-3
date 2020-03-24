4cc4085e49560989685ffa76f73dfe5c71b26d32
Agafonov Alexey <agafonov@fgosniias.ru>
2020-03-24 16:50:17 +0300

    Обновляет структуру данных для описания публикации: добавляет id и comments.

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -3,12 +3,13 @@
 const fs = require(`fs`).promises;
 const chalk = require(`chalk`);
 
-const {getRandomInt, shuffle} = require(`../../utils`);
+const {getRandomInt, shuffle, getNewId} = require(`../../utils`);
 const {ExitCode, MOCK_FILE_NAME} = require(`../../constants`);
 
 const FILE_TITLES_PATH = `./data/titles.txt`;
 const FILE_CATEGORIES_PATH = `./data/categories.txt`;
 const FILE_ANNOUNCEMENT_PATH = `./data/announcement.txt`;
+const FILE_COMMENTS_PATH = `./data/comments.txt`;
 
 
 const DEFAULT_OFFER = 1;
@@ -27,6 +28,11 @@ const FullText = {
 const Category = {
   MIN: 1,
   MAX: 5,
+};
+
+const Comment = {
+  MIN: 0,
+  MAX: 8,
 };
 
 const writeToFile = async (fileName, content) => {
@@ -55,13 +61,22 @@ const getRandomDate = () => {
   return day;
 };
 
-const generateOffers = (count, titles, announcement, categories) => (
+const getComments = (countComments, comments) => {
+  return Array(countComments).fill({}).map(() => ({
+    id: getNewId(),
+    text: shuffle(comments).slice(0, getRandomInt(Comment.MIN, Comment.MAX)).join(` `),
+  }));
+};
+
+const generateOffers = (count, titles, announcement, categories, comments) => (
   Array(count).fill({}).map(() => ({
+    id: getNewId(),
     title: titles[getRandomInt(0, titles.length - 1)],
     createDate: getRandomDate().toLocaleString(),
     announce: shuffle(announcement).slice(0, getRandomInt(Announce.MIN, Announce.MAX)).join(` `),
     fullText: shuffle(announcement).slice(0, getRandomInt(FullText.MIN, FullText.MAX)).join(` `),
     category: shuffle(categories).slice(0, getRandomInt(Category.MIN, Category.MAX)),
+    comments: getComments(getRandomInt(Comment.MIN, Comment.MAX), comments),
   }))
 );
 
@@ -69,6 +84,7 @@ const generateMocks = async (arg) => {
   const titles = await readContent(FILE_TITLES_PATH);
   const announcement = await readContent(FILE_ANNOUNCEMENT_PATH);
   const categories = await readContent(FILE_CATEGORIES_PATH);
+  const comments = await readContent(FILE_COMMENTS_PATH);
 
   const [count] = arg;
   const countOffer = Number.parseInt(count, 10) || DEFAULT_OFFER;
@@ -76,7 +92,7 @@ const generateMocks = async (arg) => {
     console.error(chalk.red(`Не больше 1000 публикаций`));
     process.exit(ExitCode.error);
   }
-  const content = JSON.stringify(generateOffers(countOffer, titles, announcement, categories));
+  const content = JSON.stringify(generateOffers(countOffer, titles, announcement, categories, comments));
   await writeToFile(MOCK_FILE_NAME, content);
 };
 
--- src/utils.js
+++ src/utils.js
@@ -1,11 +1,12 @@
 'use strict';
 
+const nanoid = require(`nanoid`);
+
 const getRandomInt = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
-
 const shuffle = (someArray) => {
   for (let i = someArray.length - 1; i > 0; i--) {
     const randomPosition = Math.floor(Math.random() * i);
@@ -14,8 +15,13 @@ const shuffle = (someArray) => {
 
   return someArray;
 };
+const getNewId = () => {
+  return nanoid(6);
+};
+
 
 module.exports = {
   getRandomInt,
   shuffle,
+  getNewId,
 };
