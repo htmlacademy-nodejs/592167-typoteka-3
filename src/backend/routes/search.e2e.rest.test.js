610bb01dd5779b5f0c2eb28fd08a423f10ac8ed7
Agafon <dragafony@gmail.com>
2020-04-30 11:16:55 +0300

    Добавляет метод encodeURI, чтоб запрос корректно работал с кириллицей.

--- src/backend/routes/search.e2e.rest.test.js
+++ src/backend/routes/search.e2e.rest.test.js
@@ -7,8 +7,15 @@ const {OK} = require(`../../constants`).HttpCode;
 
 describe(`search`, () => {
   test(`if find substring in string response should have property 'id'`, async () => {
-    const res = await request(app).get(`/api/search?query=рыба`);
+    const res = await request(app).get(encodeURI(`/api/search?query=соб`));
 
     expect(res.statusCode).toBe(OK);
+    expect(res.body[0]).toHaveProperty(`id`);
+    expect(res.body[0]).toHaveProperty(`title`);
+    expect(res.body[0]).toHaveProperty(`createDate`);
+    expect(res.body[0]).toHaveProperty(`announce`);
+    expect(res.body[0]).toHaveProperty(`fullText`);
+    expect(res.body[0]).toHaveProperty(`categories`);
+    expect(res.body[0]).toHaveProperty(`comments`);
   });
 });
