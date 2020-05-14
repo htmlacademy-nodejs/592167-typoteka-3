e6947fe045813f8ff6d75cf058981b53801c3844
Agafonov Alexey <agafonov@fgosniias.ru>
2020-05-14 13:56:48 +0300

    Добавляет в categories.e2e.test логику для добавления и удаления моковой статьи.

--- src/backend/routes/categories.e2e.test.js
+++ src/backend/routes/categories.e2e.test.js
@@ -3,12 +3,33 @@
 const request = require(`supertest`);
 const app = require(`../app`);
 
+const articleRepository = require(`../repositories/article`);
 const {OK} = require(`../../constants`).HttpCode;
+const MOCK_ARTICLE = {
+  categories: [
+    `Наука`,
+    `Музыка`,
+    `Фантастика`,
+  ],
+};
+
+const addMockArticle = () => articleRepository.save(Object.assign({}, MOCK_ARTICLE));
+const deleteMockArticle = (id) => articleRepository.remove(id);
 
 describe(`get categories`, () => {
-  test(`when get all categories status code should be OK`, async () => {
+  test(`for non-existing mock.json categories list should be empty`, async () => {
     const res = await request(app).get(`/api/categories`);
 
     expect(res.statusCode).toBe(OK);
+    expect(res.body.length === 0).toBeTruthy();
+  });
+
+  test(`categories should be 'MOCK_ARTICLE.categories'`, async () => {
+    const id = addMockArticle();
+    const res = await request(app).get(`/api/categories`);
+
+    expect(res.body).toEqual(MOCK_ARTICLE.categories);
+
+    deleteMockArticle(id);
   });
 });
