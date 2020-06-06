6f385172214db221bf880b97b754b1185056ce44
Agafon <dragafony@gmail.com>
2020-06-06 19:01:19 +0300

    Запрашивает объявления с ресурса /api/articles, для страницы "Мои публикации".

--- src/frontend/routes/my.js
+++ src/frontend/routes/my.js
@@ -3,7 +3,12 @@
 const {Router} = require(`express`);
 const router = new Router();
 
-router.get(`/`, (req, res) => {
+const axios = require(`axios`);
+const {BACKEND_URL} = require(`../../constants`);
+
+router.get(`/`, async (req, res) => {
+  const response = await axios.get(`${BACKEND_URL}/api/articles`);
+  console.log(response.data);
   res.render(`my`);
 });
 router.get(`/comments`, (req, res) => res.send(req.originalUrl));
