611a11572b741cb3c5e7df4075fa5194294ec355
Agafon <dragafony@gmail.com>
2020-06-06 19:15:23 +0300

    Запрашивает комментарии к первым 3 публикациям с ресурса /api/articles/:articleId/comments

--- src/frontend/routes/my.js
+++ src/frontend/routes/my.js
@@ -6,11 +6,20 @@ const router = new Router();
 const axios = require(`axios`);
 const {BACKEND_URL} = require(`../../constants`);
 
+const MAX_ARTICLES_COMMENTS = 3;
+
 router.get(`/`, async (req, res) => {
   const response = await axios.get(`${BACKEND_URL}/api/articles`);
   console.log(response.data);
   res.render(`my`);
 });
-router.get(`/comments`, (req, res) => res.send(req.originalUrl));
+
+router.get(`/comments`, async (req, res) => {
+  const response = await axios.get(`${BACKEND_URL}/api/articles`);
+  const articles = response.data;
+  const amountOfArticles = Math.min(MAX_ARTICLES_COMMENTS, articles.length);
+  const commentsOnArticles = articles.slice(0, amountOfArticles);
+  res.render(`comments`);
+});
 
 module.exports = router;
