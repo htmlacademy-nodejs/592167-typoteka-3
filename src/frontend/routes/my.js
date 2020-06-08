e42ec29f1e5c4afe1ac14873b82e3ddf30edda3f
Agafonov Alexey <agafonov@fgosniias.ru>
2020-06-08 15:35:53 +0300

    Меняет шаблон my.pug, чтобы он небыл статичным.

--- src/frontend/routes/my.js
+++ src/frontend/routes/my.js
@@ -10,8 +10,8 @@ const MAX_ARTICLES_COMMENTS = 3;
 
 router.get(`/`, async (req, res) => {
   const response = await axios.get(`${BACKEND_URL}/api/articles`);
-  console.log(response.data);
-  res.render(`my`);
+  const articles = response.data;
+  res.render(`my`, {articles});
 });
 
 router.get(`/comments`, async (req, res) => {
@@ -19,6 +19,7 @@ router.get(`/comments`, async (req, res) => {
   const articles = response.data;
   const amountOfArticles = Math.min(MAX_ARTICLES_COMMENTS, articles.length);
   const commentsOnArticles = articles.slice(0, amountOfArticles);
+  console.log(commentsOnArticles);
   res.render(`comments`);
 });
 
