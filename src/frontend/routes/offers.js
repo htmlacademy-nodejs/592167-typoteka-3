883644f189e6d9ce79849087816dca5fd9411178
Agafonov Alexey <agafonov@fgosniias.ru>
2020-06-08 15:34:18 +0300

    Запрашивает комментарии к первым 3 публикациям.

--- src/frontend/routes/offers.js
+++ src/frontend/routes/offers.js
@@ -1,11 +1,20 @@
 'use strict';
 
+const axios = require(`axios`);
+const { BACKEND_URL } = require(`../../constants`);
+
 const {Router} = require(`express`);
 const router = new Router();
 
 router.get(`/category/:id`, (req, res) => res.send(req.originalUrl));
-router.get(`/add`, (req, res) => res.send(req.originalUrl));
-router.get(`/edit/:id`, (req, res) => res.send(req.originalUrl));
+router.get(`/add`, (req, res) => {
+  res.render(`new-post`);
+});
+router.get(`/edit/:id`, async (req, res) => {
+  const response = await axios.get(`${BACKEND_URL}/api/articles/${req.params.id}`);
+  console.log(response.data);
+  res.send(req.originalUrl)
+});
 router.get(`/:id`, (req, res) => res.send(req.originalUrl));
 
 module.exports = router;
