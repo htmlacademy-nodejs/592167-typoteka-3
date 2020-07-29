decf3d82ea8ebaec5fd40d43707e14215a346b31
Agafon <dragafony@gmail.com>
2020-07-29 16:27:04 +0300

    Добавляет логирование для новых событий: начало установки соединения, установка соединения и ошибка во время установки соединения.

--- src/backend/db/db-connect.js
+++ src/backend/db/db-connect.js
@@ -3,6 +3,9 @@
 const Sequelize = require(`sequelize`);
 require(`dotenv`).config();
 
+const {getLogger} = require(`../logger`);
+const logger = getLogger();
+
 const sequelize = new Sequelize(`${process.env.DB_NAME}`, `${process.env.DB_USER}`, `${process.env.USER_PASSWORD}`, {
   host: `${process.env.DB_HOST}`,
   dialect: `${process.env.DIALECT}`
@@ -10,8 +13,12 @@ const sequelize = new Sequelize(`${process.env.DB_NAME}`, `${process.env.DB_USER
 
 const testConnection = async () => {
   try {
+    logger.info(`Establishing a connection to the server.`);
     await sequelize.authenticate();
+    logger.info(`The connection to the server has been established.`);
   } catch (err) {
+    console.error(`Failed to establish connection for the reason: ${err}`);
+    logger.error(`Failed to establish connection for the reason: ${err}`);
     process.exit();
   }
 };
