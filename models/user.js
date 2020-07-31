2566da88db9969f161b35386d0176bf96ff5176f
Agafonov Alexey <agafonov@fgosniias.ru>
2020-07-31 14:37:52 +0300

    Добавляет модель таблицы users.

---
+++ models/user.js
@@ -0,0 +1,35 @@
+'use strict';
+
+model.exports = (sequelize, DataTypes) => {
+  class User extends sequelize.Sequelize.Model{ }
+  User.init({
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    firstName: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  }, {
+    sequelize,
+    timestamps: true,
+    paranoid: true,
+  });
+
+  return User;
+};
