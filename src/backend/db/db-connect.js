df20cfbdaa292d7dcda41842143989c30f103cee
Agafonov Alexey <agafonov@fgosniias.ru>
2020-07-31 16:13:14 +0300

    Добавляет метод initDb для инициализации таблиц БД.

--- src/backend/db/db-connect.js
+++ src/backend/db/db-connect.js
@@ -11,9 +11,23 @@ const sequelize = new Sequelize(`${process.env.DB_NAME}`, `${process.env.DB_USER
   dialect: `${process.env.DIALECT}`
 });
 
+const Type = require(`./models/type`)(sequelize, Sequelize);
+const User = require(`./models/user`)(sequelize, Sequelize);
+const Category = require(`./models/category`)(sequelize, Sequelize);
+const Image = require(`./models/image`)(sequelize, Sequelize);
+const Comment = require(`./models/comment`)(sequelize, Sequelize);
+const AnnouncementsToCategory = require(`./models/announcements-to-category`)(sequelize, Sequelize);
+const Announcement = require(`./models/announcement`)(sequelize, Sequelize);
+
+const initDb = async () => {
+  await sequelize.sync({force: true});
+  console.info(`Структура БД успешно создана`);
+};
+
 const testConnection = async () => {
   try {
     logger.info(`Establishing a connection to the server.`);
+    // await initDb();
     await sequelize.authenticate();
     logger.info(`The connection to the server has been established.`);
   } catch (err) {
