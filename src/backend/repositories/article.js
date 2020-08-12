95bce2c8e1e716bfd5e13cbd34cdd2fab4fbd8b9
Agafonov Alexey <agafonov@fgosniias.ru>
2020-08-12 18:10:41 +0300

    Меняет логику выборки данных, чтобы данные брались из БД, для составления списка всех постов.

--- src/backend/repositories/article.js
+++ src/backend/repositories/article.js
@@ -8,43 +8,55 @@ const {db, sequelize} = require(`../db/db-connect`);
 const {MOCK_FILE_NAME} = require(`../../constants`);
 let articles = fs.existsSync(MOCK_FILE_NAME) ? JSON.parse(fs.readFileSync(MOCK_FILE_NAME)) : [];
 
-const findAll = async () => await db.Article.findAll({
-  attributes: [`id`, `title`, `announce`, `createdAt`,
-    [
-      sequelize.literal(`(
-                    SELECT image.image
-        FROM "Images" AS image
-        WHERE
-                image."articleId" = "Article".id
-        limit 1
-                )`),
-      `images.image`
-    ],
-    [
-      sequelize.literal(`(
-                    SELECT count(comment.comment)
-        FROM "Comments" as comment
-        WHERE
-                comment."articleId" = "Article".id
-        )`),
-      `comments.comment`
-    ],
-    [
-      sequelize.literal(`string_agg(c.category, ', ')`), `categories`
-    ],
-  ],
-  include: [
-    {
-      model: db.ArticlesToCategory,
-      as: `articlesToCategories`,
-      include: {
-        model: db.Category,
-        as: `articlesToCategories`,
-      }
-    },
-  ],
-  limit: 1,
-});
+// const findAll = async () => await db.Article.findAll({
+//   attributes: [`id`, `title`, `announce`, `createdAt`,
+//     [
+//       sequelize.literal(`(
+//                     SELECT image.image
+//         FROM "Images" AS image
+//         WHERE
+//                 image."articleId" = "Article".id
+//         limit 1
+//                 )`),
+//       `images.image`
+//     ],
+//     [
+//       sequelize.literal(`(
+//                     SELECT count(comment.comment)
+//         FROM "Comments" as comment
+//         WHERE
+//                 comment."articleId" = "Article".id
+//         )`),
+//       `comments.comment`
+//     ],
+//   ],
+//   include: {
+//     model: db.Category,
+//     attributes: [sequelize.literal("STRING_AGG(category, '-')")],
+//     as: `categories`,
+//   },
+//   group: [`Article.id`, `Article.title`, `Article.announce`, `Article.createdAt`,],
+//   limit: 1,
+// });
+
+const findAll = async () => {
+  const sql = `select a.id, a.title, a.announce, a."createdAt",
+       (select count(*) from "Comments" cm where cm."articleId"=a.id),
+       string_agg(c.category, ', ') as categories
+from "Articles" a
+         inner join "ArticlesToCategories" atc
+                    on a.id = atc."articleId"
+         inner join "Categories" c
+                    on atc."categoryId" = c.id
+group by a.id, a.title, a.description, a."createdAt"
+order by a."createdAt" desc;`;
+
+  const type = sequelize.QueryTypes.SELECT;
+  const articles = await sequelize.query(sql, {type});
+
+  sequelize.close();
+  return articles;
+}
 
 const findById = (id) => articles.find((el) => el.id === id);
 
