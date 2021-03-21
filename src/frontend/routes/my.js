80c3648eeba4a2111e7e554ccba6e2860534dec0
Agafon <dragafony@gmail.com>
2021-03-22 00:37:43 +0300

    Добавляет константу USER_ROLE_ADMIN для того чтобы убрать магическое число, при отрисовки блока всех публикаций автора блога.

--- src/frontend/routes/my.js
+++ src/frontend/routes/my.js
@@ -11,6 +11,9 @@ const {BACKEND_URL, MOCK_USER_ID, USER_ROLE_ADMIN} = require(`../../constants`);
 router.get(`/`, [privatePath(true)], async (req, res) => {
   const response = await axios.get(`${BACKEND_URL}/api/articles/myArticles`);
   const myArticles = response.data;
+  myArticles.userInfo = {
+    userRole: USER_ROLE_ADMIN
+  };
   res.render(`my`, {myArticles});
 });
 
