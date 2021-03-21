817b9d544543f87bbedac38995b9494ffc913087
Agafon <dragafony@gmail.com>
2021-03-22 00:23:50 +0300

    Добавляет константу USER_ROLE_ADMIN для того чтобы убрать магическое число, при отрисовки блока всех категорий в админке.

--- src/frontend/routes/categories.js
+++ src/frontend/routes/categories.js
@@ -5,11 +5,15 @@ const axios = require(`axios`);
 const router = new Router();
 const privatePath = require(`../../middleware/private`);
 
-const {BACKEND_URL} = require(`../../constants`);
+const {BACKEND_URL, USER_ROLE_ADMIN} = require(`../../constants`);
 
 router.get(`/`, [privatePath(true)], async (req, res) => {
   const categoriesList = await axios.get(`${BACKEND_URL}/api/categories?categoriesList=only`);
   const categories = categoriesList.data;
+  categories.BACKEND_URL = BACKEND_URL;
+  categories.userInfo = {
+    userRole: USER_ROLE_ADMIN
+  };
   res.render(`all-categories`, {categories});
 });
 
